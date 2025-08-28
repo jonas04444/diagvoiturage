ed7162802627b47e19dca86bd030338fabad5186
jonat <jonathan04444@gmail.com>
2025-08-28 19:41:56 +0200

    modélisation personnalisée

--- sqlite.py
+++ sqlite.py
@@ -2,6 +2,22 @@ import sqlite3
 
 conn = sqlite3.connect("dbdiaggrantt.db")
 cur = conn.cursor()
-cur.execute("INSERT INTO ")
-
 conn.close()
+
+def add_line(donnees_ligne):
+    conn = sqlite3.connect("dbdiaggrantt.db")
+    cur =  conn.cursor()
+
+    for version in donnees_ligne:
+        cur.execute("""
+            INSERT INTO Version_linge (
+                id_version, num_ligne, Version_ligne
+            ) VALUES (?, ?, ?)
+        """, (
+            version["id_version"],
+            version["num_ligne"],
+            version["Version_ligne"]
+        ))
+
+    conn.commit()
+    conn.close()
