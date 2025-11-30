8a389c1fca080688cd118b923fa75eca6776c293
jonat <jonathan04444@gmail.com>
2025-11-30 11:02:38 +0100

    test autre fenetre pour charger csv

--- tabelauCSV.py
+++ tabelauCSV.py
@@ -53,3 +53,62 @@ class TableauCSV(ctk.CTkFrame):
             command=lambda: msgbox.showinfo("exit")
         )
         exit_csv_window.pack(side="left", padx=5)
+
+    def creer_tableau(self):
+        #création tableau
+        frame_tableau = ctk.CTkFrame(self)
+        frame_tableau.pack(fill="both", expand=True, padx=10, pady=10)
+
+        style = ttk.Style()
+        style.theme_use('dark')
+
+        colonnes = ('Sélection', 'Ligne', 'Voy.', 'Début', 'Fin', 'De', 'À', 'Js srv')
+
+        self.tableau = ttk.Treeview(
+            frame_tableau,
+            columns=colonnes,
+            show='headings',
+            height=15
+        )
+
+        en_tetes={
+            'Sélection': 50,
+            'Ligne': 50,
+            'Voy.': 50,
+            'Début': 80,
+            'Fin': 80,
+            'De': 120,
+            'À': 120,
+            'Js srv': 100
+        }
+
+        for col, largeur in en_tetes.items():
+            self.tableau.column(col, width=largeur, anchor='center')
+            self.tableau.heading(col, text=col)
+
+        self.remplir_tableau()
+
+        scrollbar_y = ttk.Scrollbar(
+            frame_tableau,
+            orient='vertical',
+            command=self.tableau.yview
+        )
+
+        scrollbar_x = ttk.Scrollbar(
+            frame_tableau,
+            orient='horizontal',
+        command = self.tableau.yview
+        )
+
+        self.tableau.configure(
+            yscroll=scrollbar_y.set,
+            xscroll=scrollbar_x.set
+        )
+
+        self.tableau.grid(row=0, column=0, sticky='nsew')
+        scrollbar_y.grid(row=0, column=1, sticky='ns')
+        scrollbar_x.grid(row=1, column=0, sticky='ew')
+
+        frame_tableau.grid_rowconfigure(0, weight=1)
+        frame_tableau.grid_columnconfigure(0, weight=1)
+
