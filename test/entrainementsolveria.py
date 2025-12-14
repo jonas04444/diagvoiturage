01d72e1b350414a8477d96b249f7586ee5f86c9d
jonat <jonathan04444@gmail.com>
2025-12-14 07:53:26 +0100

    on reprend un peu les bases afin de mieux maitriser l'ia

--- test/entrainementsolveria.py
+++ test/entrainementsolveria.py
@@ -1,5 +1,14 @@
 from ortools.sat.python import cp_model
 
+class voyage:
+    def __init__(self, heure_debut, heure_fin):
+        self.hdebut = heure_debut
+        self.hfin = heure_fin
+
+def egalite(x,y,z):
+    return x + y == z
+
+
 def solvertest():
     model = cp_model.CpModel()
 
@@ -8,10 +17,11 @@ def solvertest():
     y = model.NewIntVar(0, num_val - 1, 'y')
     z = model.NewIntVar(0, num_val - 1, 'z')
 
-    model.Add(x != y)
+    #model.Add(egalite(x,y,z))
 
     solver = cp_model.CpSolver()
     status = solver.Solve(model)
+
 
     if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
         print(f"x = {solver.Value(x)}")
@@ -40,4 +50,11 @@ class SolutionPrinter(cp_model.CpSolverSolutionCallback):
     def solution_count(self) -> int:
         return self.__solution_count
 
-solvertest()
+
+voyage1 = voyage(800,900)
+voyage2 = voyage(900,1000)
+
+listes = [voyage1, voyage2]
+
+for i, v in enumerate(listes):
+    print(f"Voyage {i+1}: {v.hdebut}")
